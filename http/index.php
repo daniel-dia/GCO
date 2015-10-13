c4ea2156c08960a5ebc6197cb706940f2dccf8fb
Daniel Santos <artsjedi@hotmail.com>
2015-10-12 22:15:32 -0300

    fix charset on index

--- http/index.php
+++ http/index.php
@@ -49,9 +49,8 @@
 	header("Content-type: text/html; charset=ISO-8859-1", true);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Gerenciador Clínico Odontológico Smile Odonto - Administração Odontológica Em Suas Mãos</title>
 <link rel="SHORTCUT ICON" href="favicon.ico">
 <link href="css/smile.css" rel="stylesheet" type="text/css" />
